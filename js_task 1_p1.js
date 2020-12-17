aa407989608d996a36784a5d58aa726ed6598802
Alexander Novik <37306815+junkosher@users.noreply.github.com>
2020-12-18 02:23:26 +0300

    Add files via upload

---
+++ js_task 1_p1.js
@@ -0,0 +1,63 @@
+// PART 1
+
+// point 1
+
+// let exponentiation = Number(prompt("Введите число"));
+// alert (exponentiation + " в степени 2 равно " + exponentiation**2);
+
+// point 2
+
+// let average1 = Number(prompt("Введите первое число"));
+// let average2 = Number(prompt("Введите второе число"));
+// alert ("Среднее арифметическое числа " + average1 + " и числа " + average2 + " равно " + (average1+average2)/2);
+
+// point 3
+
+// let square = Number(prompt("Введите длину стороны квадрата"));
+// alert ("Площадь квадрата с длиной стороны " + square + " равна " + square**2);
+
+// point 4
+
+// const miles = 0.621371;
+// let kilometer = Number(prompt("Введите значение в километрах"));
+// let converter = miles*kilometer;
+// alert ("В " + kilometer + " километрах " + converter.toFixed(1) + " миль ");
+
+// point 5
+
+// let calculator1 = Number(prompt("Введите первое число"));
+// let calculator2 = Number(prompt("Введите второе число"));
+// alert ("Сумма числа " + calculator1 + " и числа " + calculator2 + " равна " + (calculator1 + calculator2));
+// alert ("Разница числа " + calculator1 + " и числа " + calculator2 + " равна " + (calculator1 - calculator2));
+// alert ("Произведение числа " + calculator1 + " и числа " + calculator2 + " равно " + (calculator1 * calculator2));
+// alert ("Частное числа " + calculator1 + " из числа " + calculator2 + " равно " + (calculator1 / calculator2));
+    
+// point 6
+
+// let number_a = Number(prompt('Для вычисления числа Х по формуле a * x + b = 0 сначала введите значение "a"'));
+// let number_b = Number(prompt('А теперь введите значение "b"'));
+// alert ("x = " + ((-number_b)/number_a));
+
+// point 7
+
+// let hours = Number(prompt("Введите текущее время (часы)"));
+// let minutes = Number(prompt("Введите текущее время (минуты)"));
+// let hours_left = 24 - hours;
+// let minutes_left = 60 - minutes;
+// if (minutes >= 1) {
+//     alert ("До конца дня осталось " + (hours_left - 1) + " часов " + " и " + minutes_left + " минут");
+// }   else {
+//     alert ("До конца дня осталось " + hours_left + " часов " + " и " + 0 + " минут");
+// }
+
+
+// hm1, point 8 
+
+// let rubles = Number(prompt("Введите количество рублей в кошельке"));
+// let kopecks = Number(prompt("Введите количество копеек в кошельке"));
+// let cost = Number(prompt("Введите цену одной шоколадки"));
+// let sum = rubles + kopecks*0.01;
+// let quantity = sum % cost;
+
+// alert ("Вы сможете купить " + quantity + " шоколадок и у вас останется " + ((sum / cost) - quantity) + " рублей");
+
